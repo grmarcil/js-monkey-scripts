9359da09b7f40d213f5867f8ae3a487334b9853c
Greg Marcil <grmarcil@gmail.com>
2015-01-28 16:21:14 -0500

    Add debt node as well

--- personal_capital_net_cash/personal_capital_net_cash.user.js
+++ personal_capital_net_cash/personal_capital_net_cash.user.js
@@ -1,43 +1,47 @@
 // ==UserScript==
 // @name         Personal Capital Net Cash Calculator
 // @namespace    http://home.personalcapital.com/
-// @version      0.2
+// @version      0.3
 // @description  Calculates and displays net cash in Personal Capital
 // @author       Greg Marcil
 // @match        https://home.personalcapital.com/*
 // @grant        none
 // ==/UserScript==
 
-function buildNetCashNode(net_cash) {
+function buildNode(value, label) {
   var node = document.createElement("div");
   $(node).addClass("netWorth");
 
-  var pretty_net_cash = net_cash.toLocaleString('en-US', { style: 'currency', currency: 'USD' });
-  var net_color;
-  if(net_cash > 0) {
-    net_color = "#40c893";
+  var pretty_value = value.toLocaleString('en-US', { style: 'currency', currency: 'USD' });
+  var color;
+  if(value > 0) {
+    color = "#40c893";
   } else {
-    net_color = "#f03240";
+    color = "#f03240";
   }
 
-  var node_contents = '<label>Net Cash</label><div class="amount" style="color:';
-  node_contents += net_color;
-  node_contents += ';">';
-  node_contents += pretty_net_cash;
-  node_contents += '</div>';
-  node.innerHTML = node_contents;
+  var contents = '<label>';
+  contents += label;
+  contents += '</label><div class="amount" style="color:';
+  contents += color;
+  contents += ';">';
+  contents += pretty_value;
+  contents += '</div>';
 
+  node.innerHTML = contents;
   return node;
 }
 
-function addNetCashNode() {
+function addNodes() {
   var cash = parseFloat($('.accountGroup.BANK').find('.accountGroupValue').text().replace("$", "").replace(",", ""));
   var debt = parseFloat($('.accountGroup.CREDIT_CARD').find('.accountGroupValue').text().replace("$", "").replace(",", ""));
   var net_cash = cash + debt;
 
-  var net_cash_node = buildNetCashNode(net_cash);
+  var debt_node = buildNode(debt, "Debt");
+  var net_cash_node = buildNode(net_cash, "Net Cash");
   var cash_node = $(".accountGroup.BANK").children(".accountGroupHeader");
-  $(net_cash_node).insertAfter(cash_node);
+  $(debt_node).insertAfter(cash_node);
+  $(net_cash_node).insertAfter(debt_node);
 }
 
 function pageReady() {
@@ -51,7 +55,7 @@ function pageReady() {
       setTimeout(function() {pageReady();}, 2000);
     }
   } else {
-    addNetCashNode();
+    addNodes();
   }
 }
 
