cab5e82bf2300285d16c765ce6192f8f0425f988
Greg Marcil <grmarcil@gmail.com>
2015-01-28 15:54:43 -0500

    Handle page/ajax load readiness checks differently to prevent jquery conflicts on login pages

--- personal_capital_net_cash/personal_capital_net_cash.user.js
+++ personal_capital_net_cash/personal_capital_net_cash.user.js
@@ -1,12 +1,11 @@
 // ==UserScript==
 // @name         Personal Capital Net Cash Calculator
 // @namespace    http://home.personalcapital.com/
-// @version      0.1
+// @version      0.2
 // @description  Calculates and displays net cash in Personal Capital
 // @author       Greg Marcil
 // @match        https://home.personalcapital.com/*
 // @grant        none
-// @require      http://code.jquery.com/jquery-latest.js
 // ==/UserScript==
 
 function buildNetCashNode(net_cash) {
@@ -41,27 +40,29 @@ function addNetCashNode() {
   $(net_cash_node).insertAfter(cash_node);
 }
 
-function runIfAccountsReady() {
-  window.PCAPCashRetryCounter += 1;
+function pageReady() {
   // Personal Capital loads accounts dynamically on page load
   // Kind of a hack, but it's the best quick solution I could find
   // without digging too deep in their js
-  if(window.PCAP && $.isEmptyObject(window.PCAP.refreshingAccounts)) {
-    addNetCashNode();
-  } else {
-    clearTimeout(window.retryPCAPCashTimeout);
-    // just a check to prevent infinite retries
-    if(window.PCAPCashRetryCounter < 10) {
-      window.retryPCAPCashTimeout = setTimeout(function() {runIfAccountsReady();}, 3000);
+  window.PCAPCashRetryCounter += 1;
+  if(typeof window.PCAP === 'undefined' || typeof window.PCAP.refreshingAccounts === "undefined" || !($.isEmptyObject(window.PCAP.refreshingAccounts))) {
+    // don't allow infinite retries
+    if(window.PCAPCashRetryCounter < 5) {
+      setTimeout(function() {pageReady();}, 2000);
     }
+  } else {
+    addNetCashNode();
   }
 }
 
 function main() {
-  $(document).ready(function() {
-    window.PCAPCashRetryCounter = 0;
-    runIfAccountsReady();
-  });
+  window.PCAPCashRetryCounter = 0;
+  if(typeof $ === 'undefined') {
+    setTimeout(function() {main();}, 500);
+  } else {
+    pageReady();
+  }
 }
 
 main();
+
